08be5ef347cd8ff7982b867b87d40bc286ab06a1
esturenatalia <esturenatalia14@gmail.com>
2022-12-03 00:06:07 +0700

    postest20

---
+++ src/test/java/runners/Runner_20.java
@@ -0,0 +1,9 @@
+package runners;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/java/features",
+        glue="stepdefs")
+public class Runner_20 extends AbstractTestNGCucumberTests{
+}
